e781707052ec6ad7887ca7fcfd1fe09de9944846
BrakeDude <lockbrake@gmail.com>
2025-05-29 01:16:57 +0300

    Future changelog

--- lua/core/dss/changelog.lua
+++ lua/core/dss/changelog.lua
@@ -1,5 +1,12 @@
 local modname = "Restored Hearts"
 
+DeadSeaScrollsMenu.AddChangelog(modname, "v1.1", [[{FSIZE2}fixed
+illusion hearts not giving
+transformation in vanilla
+repenentance and repentance+
+without restored collection]],
+{"restored", "hearts", "", "v1.1", "release"}, false, false)
+
 DeadSeaScrollsMenu.AddChangelog(modname, "v1.0", [[{FSIZE3}restored hearts
 {{FSIZE3}}is out
 
