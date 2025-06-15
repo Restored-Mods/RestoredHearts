8fccf8c806e30a2cce3a344938c79e4e7899bd58
BrakeDude <lockbrake@gmail.com>
2025-06-15 15:38:07 +0300

    Hide Illusion transformation items in encyclopedia

--- lua/mod_compat/encyclopedia/encyclopedia.lua
+++ lua/mod_compat/encyclopedia/encyclopedia.lua
@@ -4,4 +4,28 @@ end
 
 local Wiki = include("lua.mod_compat.encyclopedia.wiki")
 
+local TransformationItems = {
+	Isaac.GetItemIdByName("Spun transform"),
+	Isaac.GetItemIdByName("Mom transform"),
+	Isaac.GetItemIdByName("Guppy transform"),
+	Isaac.GetItemIdByName("Fly transform"),
+	Isaac.GetItemIdByName("Bob transform"),
+	Isaac.GetItemIdByName("Mushroom transform"),
+	Isaac.GetItemIdByName("Baby transform"),
+	Isaac.GetItemIdByName("Angel transform"),
+	Isaac.GetItemIdByName("Devil transform"),
+	Isaac.GetItemIdByName("Poop transform"),
+	Isaac.GetItemIdByName("Book transform"),
+	Isaac.GetItemIdByName("Spider transform"),
+}
+
+for _, item in ipairs(TransformationItems) do
+	Encyclopedia.AddItem({
+		ModName = "Compliance",
+		Class = "Compliance",
+		ID = item,
+		Hide = true,
+	})
+end
+
 RestoredHearts.Enums.Wiki = Wiki
